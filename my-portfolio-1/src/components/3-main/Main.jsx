fd9da2188d4642b4cbbcd5375d43c17cf4369ac4
mohamadhayirmohamad <muha23k6m@gmail.com>
2025-01-27 23:24:25 +0300

    Contact us push

--- my-portfolio-1/src/components/3-main/Main.jsx
+++ my-portfolio-1/src/components/3-main/Main.jsx
@@ -44,10 +44,10 @@ const Main = () => {
 
                                 </div>
                             </article>
-                        );
+                        )
 
 
-                    })};
+                    })}
 
 
 
--- my-portfolio-1/src/components/4-contact/Contact.jsx
+++ my-portfolio-1/src/components/4-contact/Contact.jsx
@@ -1,10 +1,39 @@
 
-
 import './Contact.css'
 
-const Contact = () =>{
-    return(
-        <>contact</>
+const Contact = () => {
+    return (
+        <section className='contact-us '>
+            <h1 className='title '>
+                <span className='icon-envelope'></span>
+                Contact us
+            </h1>
+            <p className='sub-title'>Contact us for more information and Get notified when l publish something new.</p>
+
+            <div className="flex">
+                <form className='' >
+                    <div  className='flex'>
+                        <label htmlFor="email">Email Address:</label>
+                        <input required type="email" name="" id="email" />
+
+                    </div>
+
+                    <div className='flex' style={{marginTop:"25px"}}>
+                        <label htmlFor="message">your message:</label>
+                        <textarea required  name="" id="message"></textarea>
+                    </div>
+
+                    <button className='submit'>submit</button>
+                </form>
+
+                <div className="animation border"> animation  </div>
+
+            </div>
+
+
+
+
+        </section>
     )
 }
 export default Contact
