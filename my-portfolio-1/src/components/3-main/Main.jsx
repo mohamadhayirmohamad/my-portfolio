579e5028a5e48697ddcc9f23eb3de0096250bf6f
mohamadhayirmohamad <muha23k6m@gmail.com>
2025-01-29 23:28:32 +0300

    code push

--- my-portfolio-1/src/components/3-main/Main.jsx
+++ my-portfolio-1/src/components/3-main/Main.jsx
@@ -2,22 +2,7 @@
 
 import { useState } from 'react'
 import './Main.css'
-const myProjects = [
-
-
-
-
-    { projectTitle: "css project", category: ["css"], imgPath: "./wp5134484.webp" },
-
-    { projectTitle: "react project", category: ["react"], imgPath: "./wp5134484.webp" },
-
-    { projectTitle: "jss project", category: ["jss"], imgPath: "./wp5134484.webp" },
-
-    { projectTitle: "boot project", category: ["boot"], imgPath: "./wp5134484.webp" },
-
-
-
-]
+import { myProjects } from './myprojects';
 
 
 
